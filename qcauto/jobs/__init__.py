4651a16281ffb47c36c8280d46cd3c03cd2dd55e
Peter Spackman <peterspackman@fastmail.com>
2016-08-18 15:15:35 +0800

    Minor additions

--- qcauto/jobs/__init__.py
+++ qcauto/jobs/__init__.py
@@ -1,2 +1,13 @@
 import attr
 from .gaussian import GaussianJob
+
+class Job(object):
+
+    def before_run(self):
+        pass
+
+    def args(self):
+        return []
+
+    def after_run(self):
+        pass
--- qcauto/jobs/runners/localrunner.py
+++ qcauto/jobs/runners/localrunner.py
@@ -11,6 +11,7 @@ def path_is_executable(instance, attribute, value):
 
 @attr.s
 class LocalRunner:
+    """ Currently totally sequential """
     executable_path = attr.ib(validator=path_is_executable)
     _success = attr.ib(default=False)
     _jobs = attr.ib(default=attr.Factory(list))
@@ -22,12 +23,9 @@ class LocalRunner:
         while not len(self._jobs) == 0:
             job = self._jobs.pop(0)
             log.debug('Starting {} '.format(job.name))
-
             job.before_run()
-
             command = self.executable_path + ' ' + ' '.join(job.args())
             job.success = execute(command)
-
             job.after_run()
             log.debug('{} success: {}'.format(command, self._success))
             yield job
