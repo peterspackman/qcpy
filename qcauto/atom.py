eb35ac292284d0dffd23fa341e439f3d7b1dc13e
Peter Spackman <peterspackman@fastmail.com>
2016-10-12 10:36:55 +0800

    Refactored atom class

--- qcauto/atom.py
+++ qcauto/atom.py
@@ -1,22 +1,33 @@
-import attr
-from qcauto.coordinates import Coordinates
-from qcauto.element import Element, periodictable
-from attr.validators import instance_of
+"""
+Store all the desired info about an atom in a molecule etc
+"""
+from qcauto.element import periodictable
 
 
-@attr.s
 class Atom:
-    element = attr.ib(validator=instance_of(Element))
-    center = attr.ib(validator=instance_of(Coordinates))
+    """TODO Atom documentation"""
+    _element = None
+    _center = None
+    
+    def __init__(self, element, center):
+        self._element = element
+        self._center = center
 
     @staticmethod
     def from_symbol_and_location(symbol, center):
         """ Create a new Atom from a given atomic symbol and coordinates
-        >>> Atom.from_symbol_and_location('Ca', Coordinates(0, 0, 0))
-        Atom(element=Ca, center=Coordinates(x=0.0, y=0.0, z=0.0))
-        >>> Atom.from_symbol_and_location('Bo', Coordinates(1, 1, 0))
-        Traceback (most recent call last):
-         ...
-        KeyError: "No such element 'Bo'"
         """
         return Atom(periodictable[symbol], center=center)
+
+    @property
+    def element(self):
+        return self._element
+    
+    @property
+    def center(self):
+        return self._center
+    
+    @property
+    def covalent_radius(self):
+        """This atom's covalent radius in angstroms"""
+        return self.element.covalent_radius
--- qcauto/tests/test_geometry.py
+++ qcauto/tests/test_geometry.py
@@ -1,3 +1,6 @@
+"""
+Geometry tests
+"""
 from unittest import TestCase
 from qcauto.element import O, H
 from qcauto.atom import Atom
@@ -8,20 +11,24 @@ H2O = Geometry(atoms=[
     Atom(O, center=Coordinates(0.0, 0.0, 0.11779)),
     Atom(H, center=Coordinates(0.0, 0.7554530, -0.471161)),
     Atom(H, center=Coordinates(0.0, -0.7554530, -0.471161))],
-    charge=0, multiplicity=1)
+               charge=0, multiplicity=1)
 
 
 class TestGeometry(TestCase):
+    """Test case for Geometry object"""
     geom = H2O
 
     def test_elements(self):
+        """Check that the elements are correct"""
         assert self.geom.elements() == [O, H, H]
 
     def test_molecular_formula(self):
+        """Check that the calculated molecular formula is correct"""
         assert self.geom.molecular_formula() == "H2O"
 
     def test_as_lines(self):
+        """Check output as lines"""
         assert self.geom.as_lines() == [
-                "O    0.0000000   0.0000000   0.1177900",
-                "H    0.0000000   0.7554530  -0.4711610",
-                "H    0.0000000  -0.7554530  -0.4711610"]
+            "O    0.0000000   0.0000000   0.1177900",
+            "H    0.0000000   0.7554530  -0.4711610",
+            "H    0.0000000  -0.7554530  -0.4711610"]
